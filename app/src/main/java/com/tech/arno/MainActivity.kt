cb0112b8881f544a66e4d2a70e824f3cbfda5f7e
xuxin14 <xuxin14@staff.sina.com>
2022-09-07 10:06:27 +0800

    Feature: 动画效果演示

--- app/src/main/java/com/tech/arno/MainActivity.kt
+++ app/src/main/java/com/tech/arno/MainActivity.kt
@@ -3,12 +3,21 @@ package com.tech.arno
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.tech.arno.ui.theme.ArnoComposePlaygroundTheme
 
@@ -39,5 +48,31 @@ fun Greeting(name: String) {
 fun DefaultPreview() {
     ArnoComposePlaygroundTheme {
         Greeting("Android")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SimpleSection() {
+    Card {
+        var expanded by remember { mutableStateOf(false) }
+        Column(
+            Modifier
+                .clickable { expanded = !expanded }
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.jetpack_compose_logo),
+                contentDescription = null
+            )
+            AnimatedVisibility(visible = expanded) {
+                Text(
+                    text = "Jetpack Compose",
+                    style = MaterialTheme.typography.h2
+                )
+            }
+        }
+
     }
 }
