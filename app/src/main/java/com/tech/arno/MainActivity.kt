603e65fa32416b6c5fff4d83ba3f3aed0d9b8624
xuxin14 <xuxin14@staff.sina.com>
2022-09-07 10:15:40 +0800

    Feature: 介绍重组特性

--- app/src/main/java/com/tech/arno/MainActivity.kt
+++ app/src/main/java/com/tech/arno/MainActivity.kt
@@ -3,22 +3,7 @@ package com.tech.arno
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
+import com.tech.arno.demo.SimpleAnimationSection
 import com.tech.arno.ui.theme.ArnoComposePlaygroundTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,53 +11,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ArnoComposePlaygroundTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                SimpleAnimationSection()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ArnoComposePlaygroundTheme {
-        Greeting("Android")
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun SimpleSection() {
-    Card {
-        var expanded by remember { mutableStateOf(false) }
-        Column(
-            Modifier
-                .clickable { expanded = !expanded }
-                .fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Image(
-                painter = painterResource(id = R.drawable.jetpack_compose_logo),
-                contentDescription = null
-            )
-            AnimatedVisibility(visible = expanded) {
-                Text(
-                    text = "Jetpack Compose",
-                    style = MaterialTheme.typography.h2
-                )
-            }
-        }
-
-    }
-}
---
+++ app/src/main/java/com/tech/arno/demo/ShowCompose.kt
@@ -0,0 +1,60 @@
+package com.tech.arno.demo
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.tech.arno.R
+
+@Preview(showBackground = true)
+@Composable
+fun SimpleAnimationSection() {
+    Card {
+        var expanded by remember { mutableStateOf(false) }
+        Column(
+            Modifier
+                .clickable { expanded = !expanded }
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.jetpack_compose_logo),
+                contentDescription = null
+            )
+            AnimatedVisibility(visible = expanded) {
+                Text(
+                    text = "Jetpack Compose",
+                    style = MaterialTheme.typography.h2
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ShowCompose() {
+    var score by remember { mutableStateOf(0) }
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Score: $score")
+        Spacer(Modifier.height(20.dp))
+        Button(onClick = { score++ }) {
+            Text(text = "Click me")
+        }
+    }
+}
