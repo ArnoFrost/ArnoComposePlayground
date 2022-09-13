4d32f81cbd648b6bad5a2da9ba76916c5de251a8
xuxin14 <xuxin14@staff.sina.com>
2022-09-13 11:28:29 +0800

    WIP:添加模拟灵动岛动画中

--- app/src/main/java/com/tech/arno/MainActivity.kt
+++ app/src/main/java/com/tech/arno/MainActivity.kt
@@ -18,11 +18,6 @@ import kotlinx.coroutines.coroutineScope
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        var isLineRoundIslandBig by mutableStateOf(false)
-        var isLineRoundIsLandAuto by mutableStateOf(false)
-
-        var isBigRoundIslandBig by mutableStateOf(false)
-        var isBigRoundIslandAuto by mutableStateOf(false)
 
         setContent {
             Column(
@@ -31,31 +26,7 @@ class MainActivity : ComponentActivity() {
                     .padding(16.dp),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Row(verticalAlignment = Alignment.CenterVertically) {
-                    Text("条幅通知")
-                    Spacer(Modifier.width(16.dp))
-                    Text("自动：")
-                    Checkbox(checked = isLineRoundIsLandAuto, onCheckedChange = {
-                        isLineRoundIsLandAuto = !isLineRoundIsLandAuto
-                    })
-                }
-                Spacer(Modifier.height(16.dp))
-                LineRoundIsland(isBig = isLineRoundIslandBig, isAuto = isLineRoundIsLandAuto) {
-                    isLineRoundIslandBig = !isLineRoundIslandBig
-                }
-                Spacer(Modifier.height(16.dp))
-                Row(verticalAlignment = Alignment.CenterVertically) {
-                    Text("卡片通知")
-                    Spacer(Modifier.width(16.dp))
-                    Text("自动：")
-                    Checkbox(checked = isBigRoundIslandAuto, onCheckedChange = {
-                        isBigRoundIslandAuto = !isBigRoundIslandAuto
-                    })
-                }
-                Spacer(Modifier.height(16.dp))
-                BigRoundIsland(isBigRoundIslandBig, isAuto = isBigRoundIslandAuto) {
-                    isBigRoundIslandBig = !isBigRoundIslandBig
-                }
+
             }
         }
     }
--- app/src/main/java/com/tech/arno/demo/AnimateDemo.kt
+++ app/src/main/java/com/tech/arno/demo/AnimateDemo.kt
@@ -3,48 +3,90 @@ package com.tech.arno.demo
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
+@Preview(showBackground = true)
 @Composable
-fun TestAnimateState(isBig: Boolean, onBoxClick: () -> Unit) {
-    val size by animateDpAsState(if (isBig) 196.dp else 96.dp) //animateXXXAsState
-    Box(modifier = Modifier
-        .size(size)
-        .background(Color.Cyan)
-        .clickable { onBoxClick.invoke() })
+fun PreviewLine() {
+    var isBig by remember { mutableStateOf(false) }
+    val isAuto = true
+    val duration = 1500L
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+//        Row(verticalAlignment = Alignment.CenterVertically) {
+//            Text("条幅通知")
+//            Spacer(Modifier.width(16.dp))
+//            Text("自动：")
+//            Checkbox(checked = isAuto, onCheckedChange = {
+//                isAuto = !isAuto
+//            })
+//        }
+//        Spacer(Modifier.height(16.dp))
+        LineRoundIsland(isBig = isBig, isAuto = isAuto, duration) {
+            isBig = !isBig
+        }
+    }
+
 }
 
 @Composable
-fun LineRoundIsland(isBig: Boolean, isAuto: Boolean, onBoxClick: () -> Unit) {
+fun LineRoundIsland(isBig: Boolean, isAuto: Boolean, duration: Long, onBoxClick: () -> Unit) {
     val scope = rememberCoroutineScope()
     val size by animateDpAsState(if (isBig) 196.dp else 24.dp) //animateXXXAsState
+
+    var isClickable by remember { mutableStateOf(true) }
     Card(shape = RoundedCornerShape(24.dp)) {
         Box(modifier = Modifier
             .height(24.dp)
             .width(size)
             .background(Color.Black)
-            .clickable {
+            .clickable(enabled = isClickable) {
                 onBoxClick.invoke()
                 if (isAuto) {
+                    isClickable = false
                     scope.launch {
-                        delay(3000)
+                        delay(duration)
                         onBoxClick.invoke()
+                        isClickable = true
                     }
                 }
             })
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewBig() {
+    var isBig by remember { mutableStateOf(false) }
+    var isAuto by remember { mutableStateOf(false) }
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+//        Row(verticalAlignment = Alignment.CenterVertically) {
+//            Text("条幅通知")
+//            Spacer(Modifier.width(16.dp))
+//            Text("自动：")
+//            Checkbox(checked = isAuto, onCheckedChange = {
+//                isAuto = !isAuto
+//            })
+//        }
+//        Spacer(Modifier.height(16.dp))
+        BigRoundIsland(isBig = isBig, isAuto = isAuto) {
+            isBig = !isBig
+        }
     }
 }
 
